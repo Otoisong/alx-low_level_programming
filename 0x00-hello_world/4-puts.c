e34375b9508227a26b92987350e5f2acd99817e1
Otoisong <tolysong4best@gmail.com>
2022-09-09 15:59:08 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 /**
- * main - block
- * Return: 0
+ * main -entry point
+ *
+ * Return: 0 after printing the function
  */
+#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a  multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
