b948d2b4783fb0e6f8e7ce9118f66b89b37b9afb
Otoisong <tolysong4best@gmail.com>
2022-09-09 11:56:01 -0700

    .c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main  -entry point
+ *
+ * Return: 0 after printing the function 
+ */
+#include <stdio.h>
+int main(void)
+{
+		puts("\"
+Programming is like building a multilingual puzzle"
+);
+						return (0);
+}
