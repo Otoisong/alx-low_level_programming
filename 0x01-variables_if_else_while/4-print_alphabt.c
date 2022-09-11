4215996bad45cdc621ab8d74e9770014e0aae72f
Otoisong <tolysong4best@gmail.com>
2022-09-11 15:09:50 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; leter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
