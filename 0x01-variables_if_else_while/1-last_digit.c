deae3968d3e38ca465a9314c2256ccd44c75abfb
Otoisong <tolysong4best@gmail.com>
2022-09-11 13:39:32 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints the last digit of a randomly generated nunmber
- * 	and whether it is greater than 5, less than 6, or 0.
+ *	and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
