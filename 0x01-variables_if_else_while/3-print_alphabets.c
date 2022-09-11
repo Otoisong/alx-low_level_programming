3154bdf84c9155090ce723620f93b0f338dc8a3d
Otoisong <tolysong4best@gmail.com>
2022-09-11 14:51:28 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z' letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
