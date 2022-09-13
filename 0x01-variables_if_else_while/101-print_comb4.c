6ad76306ff9b2a51a4e32a1fc6f67b0fba30d9d9
Otoisong <tolysong4best@gmail.com>
2022-09-12 21:59:20 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all possible combinations of three different digits,
- * 	in ascending order, separated by a comma followed by a space.
+ *	in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
