b9063728bfa3edf5edc7da49a0bb26d8398aafdb
Otoisong <tolysong4best@gmail.com>
2022-09-18 07:47:39 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,8 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		putchar(letter);
 
-	_putchar('\n');
+	putchar('\n');
 
-	return (0);
 }
