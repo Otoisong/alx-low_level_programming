e2deaf6c45c07210e92d4ca77c9eb260e73e92f0
Otoisong <tolysong4best@gmail.com>
2022-09-19 15:46:18 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "mian.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: An integer/pointer to be swapped
+ * @b: Another integer/pointer to be swapped
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int isong;
+
+	isong = *a;
+	*a = *b;
+	*b = isong;
+}
