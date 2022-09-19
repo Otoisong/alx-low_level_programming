d6e6a492450a35fd43e5c091036bdcaee9b2d83b
Otoisong <tolysong4best@gmail.com>
2022-09-19 16:39:07 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Lenght of string to be counted
+ */
+int _strlen(char *s)
+{
+	int m = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		m++;
+	}
+	return (m);
+}
