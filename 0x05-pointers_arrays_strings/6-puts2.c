4af9e5e1cf6a3782ca035ee1c2fa942d6507b696
Otoisong <tolysong4best@gmail.com>
2022-09-21 03:30:35 -0700

    taxk 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * puts2 - prints every other character of a string, starting with the first character.
+ * puts2 - prints every other character of a string,
+ *		starting with the first character.
  * @str: type
  *
  */
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <main.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
