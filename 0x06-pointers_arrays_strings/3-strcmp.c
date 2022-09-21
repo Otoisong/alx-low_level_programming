a983d6eb3312b81f30b5c3849ff57710bdfc6565
Otoisong <tolysong4best@gmail.com>
2022-09-21 15:58:25 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: first string.
+ * @s2: second string
+ *
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	nt i = 0, oty = 0;
+
+	while (oty == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		oty = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (oty);
+}
