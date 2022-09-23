fd383e5997c13f0d5e0ebefbe94ea6be99e70d50
Otoisong <tolysong4best@gmail.com>
2022-09-22 21:39:20 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string usig rod13
+ * @s: the string
+ *
+ * Return: returns address of s
+ */
+char *rot13(char *)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
