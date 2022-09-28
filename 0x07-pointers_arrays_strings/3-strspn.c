4fcf712c3d35c4a192028f470b7f8d48808810c9
Otoisong <tolysong4best@gmail.com>
2022-09-28 14:25:18 -0700

    Just my work

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: first string
+ * @accept: second string
+ *
+ * Return: unsigned int
+ */
+:
---
+++ 0x07-pointers_arrays_strings/testing_code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main( )
+{
+	int i;
+
+	{
+		printf("Enter Number:\n");
+		scanf("%d\n", &i);
+	}
+
+	if (i <= 10)
+	{
+		printf("Imabong my Love\n");
+	}
+	return (0);
+}
