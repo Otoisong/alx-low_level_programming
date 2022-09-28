a558a1e3ecc92ed8acbd799f81f37dcb0f3a7ffa
Otoisong <tolysong4best@gmail.com>
2022-09-28 15:03:25 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,4 +7,26 @@
  *
  * Return: unsigned int
  */
-:
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	 int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
+}
