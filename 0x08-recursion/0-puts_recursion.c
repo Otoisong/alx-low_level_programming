30d8e14cf0216d8b9ea117316d563c537c83c900
Otoisong <tolysong4best@gmail.com>
2022-10-01 06:07:44 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+0x08. C - Recursion
+
+* Recursion is the process where a function calls on itself.
+* To avoid infinite loop when calling on itself, we need to defind a base
+* The base definds how the function call will end/exit the loop
