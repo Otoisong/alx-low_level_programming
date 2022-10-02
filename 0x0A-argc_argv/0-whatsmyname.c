ba406665234642233f52898c8430db8a7faa7ab7
Otoisong <tolysong4best@gmail.com>
2022-10-02 08:26:08 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print the name of program
+ * @argc: number of argument past to the function
+ * @argv: the vector of the argument past to the prgram
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
