236d2fe239edf15d553fd79ca0bae13d1e2c26e9
Otoisong <tolysong4best@gmail.com>
2022-10-02 20:56:57 -0700

    edited

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,7 @@
 #include "main.h"
 
-/**
- * main - Print strings
- *
- * Return: always 0.
- */
 int main(void)
 {
-	_puts("At the end of the day, my goal was to be the best hacker");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
