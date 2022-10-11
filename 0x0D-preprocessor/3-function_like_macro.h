87de0b3e0a156d69bc081f8b36d5fbe10bd8afdd
Otoisong <tolysong4best@gmail.com>
2022-10-10 19:02:23 -0700

    fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_
 #define FUNCTION_LIKE_MACRO_
 
-/**
- * File: 3-function_like_macro.h
- * Auth: Otobong Isong
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
-
 #define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
-#endif
+
+#endif /*_FUNCTION_LIKE_MACRO_H*/
