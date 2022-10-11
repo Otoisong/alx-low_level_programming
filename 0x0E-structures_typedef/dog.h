d6e4bd6fb69c78eec87dcabcc02e06b309e88f69
Otoisong <tolysong4best@gmail.com>
2022-10-10 19:28:52 -0700

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,6 @@
 #define _DOG_H_
 
 /**
- * Author: Otobong Isong
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
@@ -21,7 +20,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
